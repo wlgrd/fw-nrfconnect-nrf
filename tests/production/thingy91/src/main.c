504952dd5f176dcfd51699d10b2e788eb055ac39
Christian Wilgaard <christian.wilgaard@nordicsemi.no>
2019-05-10 10:30:51 +0200

    tests: thingy91: Removes ztest and improved bme680 test results

--- tests/production/thingy91/src/main.c
+++ tests/production/thingy91/src/main.c
@@ -4,14 +4,26 @@
  * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
  */
 
-#include <ztest.h>
 #include <sensor.h>
 #include <nrf_cloud.h>
 #include <gpio.h>
 #include <dk_buttons_and_leds.h>
 #include <device.h>
 #include <pwm.h>
-#include <SEGGER_RTT.h>
+
+#define prod_assert_equal(a, b, err, msg) if(a != b) { \
+                                        printk("Error at %s, line %d -  %s\r\n", __func__, __LINE__, msg); \
+                                        return err; \
+                                        }
+
+#define prod_assert_not_equal(a, b, err, msg) if(a == b) { \
+                                        printk("Error at %s, line %d -  %s\r\n", __func__, __LINE__, msg); \
+                                        return err; \
+                                        }
+#define prod_assert_not_null(a, err, msg) if(a == NULL) { \
+                                        printk("Error at %s, line %d -  %s\r\n", __func__, __LINE__, msg); \
+                                        return err; \
+                                        }
 
 #define BUTTON_1		BIT(0)
 #define BUTTON_2		BIT(1)
@@ -33,6 +45,7 @@
 #define DUTY_CYCLE      (PERIOD / 4)                    //> Maximum 50% Duty cycle due to bug
 
 #if defined(ENABLE_RTT_CMD_GET)
+#include <SEGGER_RTT.h>
 static u8_t     m_rtt_keys[20];
 static u8_t     m_rtt_rx_keyindex = 0;
 bool            m_test_params_received = false;
@@ -40,7 +53,7 @@ static u8_t     msg[3];
 #endif
 static u8_t     button_test_timeout = 80;
 bool            all_tests_succeeded = true;
-
+struct          sensor_value temp;
 /* Blocking call that returns when button has changed state */
 static void wait_for_new_btn_state(void)
 {
@@ -51,97 +64,124 @@ static void wait_for_new_btn_state(void)
 	dk_set_leds(LEDS_RED);
 	while ((state == newstate) && (timeout < button_test_timeout)) {
 		newstate = dk_get_buttons();
-		// timeout++;
 		k_sleep(100);
 	}
         return;
 }
-static void ADXL372(void)
+
+static void run_test(int (*test)(void), char * testname)
+{
+        int ret;
+        printk("============================\r\n");
+        printk("Starting test: %s\r\n", testname);
+        ret = test();
+        if(ret != 0) {
+                all_tests_succeeded = false;
+                printk("Failed: %s\r\n", testname);
+                return;
+        }
+        k_sleep(200);
+        printk("Succeeded: %s\r\n", testname);
+}
+
+static int pca20035_ADXL372(void)
 {
         dk_set_leds(LEDS_RED);
 	struct device *dev;
-	dev = device_get_binding(__func__);
-	if(dev == NULL) all_tests_succeeded = false;
-	zassert_not_null(dev, "Failed to get %s\n", __func__);
+	dev = device_get_binding("ADXL372");
+        prod_assert_not_null(dev, -ENODEV, "Failed to get binding");
         dk_set_leds(DK_NO_LEDS_MSK);
+        return 0;
 }
 
-static void ADXL362(void)
+static int pca20035_ADXL362(void)
 {
         dk_set_leds(LEDS_BLUE);
-	struct device *dev;
-	dev = device_get_binding(__func__);
-	if(dev == NULL) all_tests_succeeded = false;
-	zassert_not_null(dev, "Failed to get %s \n", __func__);
+	volatile struct device *dev;
+	dev = device_get_binding("ADXL362");
+        prod_assert_not_null(dev, -ENODEV, "Failed to get binding");
         dk_set_leds(DK_NO_LEDS_MSK);
+        return 0;
 }
 
-static void BME680(void)
+static int pca20035_BME680(void)
 {
+        int err = 0;
+
         dk_set_leds(LEDS_GREEN);
 	struct device *dev;
-	dev = device_get_binding(__func__);
-	if(dev == NULL) all_tests_succeeded = false;
-	zassert_not_null(dev, "Failed to get %s \n", __func__);
+	dev = device_get_binding("BME680");
+        // prod_assert_not_null(dev, "Failed to get sensor");
+        prod_assert_not_null(dev, -ENODEV, "Failed to get binding");
+        err = sensor_sample_fetch_chan(dev, SENSOR_CHAN_ALL);
+        prod_assert_equal(err, 0, -EIO, "Failed to fetch sensor data");
+
+        err = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+        prod_assert_equal(err, 0, -EIO, "Failed to get temperature");
+        printk("BME680 temperature: %d.%d\r\n", temp.val1, temp.val2);
+
+        err = sensor_channel_get(dev, SENSOR_CHAN_PRESS, &temp);
+        prod_assert_equal(err, 0, -EIO, "Failed to get pressure");
+        printk("BME680 pressure: %d.%d kPa\r\n", temp.val1, temp.val2);
+
+        err = sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &temp);
+        prod_assert_equal(err, 0, -EIO, "Failed to get humidity");
+        printk("BME680 humidity: %d.%d %%RH\r\n", temp.val1, temp.val2);
+
+        err = sensor_channel_get(dev, SENSOR_CHAN_GAS_RES, &temp);
+        prod_assert_equal(err, 0, -EIO, "Failed to get gas");
+        printk("BME680 gas resistance: %d ohms\r\n", temp.val1);
+
         dk_set_leds(DK_NO_LEDS_MSK);
+        return 0;
 }
-static void BH1749(void)
+static int pca20035_BH1749(void)
 {
 	struct device *dev;
-	dev = device_get_binding(__func__);
-	if(dev == NULL) all_tests_succeeded = false;
-	zassert_not_null(dev, "Failed to get %s \n", __func__);
+	dev = device_get_binding("BH1749");
+	prod_assert_not_null(dev, -ENODEV, "Failed to get binding");
+        return 0;
 }
 
-static void test_button(void)
+static int pca20035_test_button(void)
 {
 	u32_t volatile state, newstate, timeout = 0, button_test_timeout = 200000;
-	PRINT("[ACTION]: Please press the button ...\n");
+	printk("[ACTION]: Please press the button ...\n");
 	state = dk_get_buttons();
-        newstate = state;	
+        newstate = state;
 	dk_set_leds(LEDS_RED);
 	while ((state == newstate) && (timeout < button_test_timeout)) {
 		newstate = dk_get_buttons();
 		timeout++;
 		k_sleep(1);
 	}
-	PRINT("timeout: %d \n", timeout);
-        all_tests_succeeded = ( (timeout != button_test_timeout) ? all_tests_succeeded : false);
-	zassert_not_equal(timeout, button_test_timeout, "Button test timed out\n");
+	printk("timeout: %d \n", timeout);
+        prod_assert_not_equal(timeout, button_test_timeout, -ETIMEDOUT, "Button test timed out");
 	dk_set_leds(DK_NO_LEDS_MSK);
+        return 0;
 }
 
-static void test_buzzer(void)
+static int pca20035_test_buzzer(void)
 {
         volatile int err_code = 0xFF;
-        static struct device *pwm_dev;
+        static struct device *dev;
         static u32_t period = PERIOD;
         static u32_t duty_cycle = DUTY_CYCLE;
-        pwm_dev = device_get_binding(DT_NORDIC_NRF_PWM_PWM_0_LABEL);
+        dev = device_get_binding(DT_NORDIC_NRF_PWM_PWM_0_LABEL);
+        prod_assert_not_null(dev, -ENODEV, "Failed to get binding");
         printk("Turning buzzer ON\n");
-        if (!pwm_dev) {
-                zassert_unreachable("Cannot bind pwm device");
-		all_tests_succeeded = false;
-        }
-        err_code = pwm_pin_set_usec(pwm_dev, BUZZER_PIN,
+        err_code = pwm_pin_set_usec(dev, BUZZER_PIN,
                                 period, duty_cycle);
-        if (err_code) {
-                zassert_unreachable("Unable to turn buzzer ON\n");
-		all_tests_succeeded = false;
-                return;
-        }
+        prod_assert_equal(err_code, 0, -EIO, "Failed to set pwm pin");
         k_sleep(1500);
         printk("Turning buzzer OFF\n");
-        err_code = pwm_pin_set_usec(pwm_dev, BUZZER_PIN,
+        err_code = pwm_pin_set_usec(dev, BUZZER_PIN,
                                 period, 0);
-        if (err_code) {
-                zassert_unreachable("Unable to turn buzzer OFF\n");
-		all_tests_succeeded = false;
-                return;
-        }
+        prod_assert_equal(err_code, 0, -EIO, "Failed to clear pwm pin");
+        return 0;
 }
 
-static void button_handler(u32_t buttons, u32_t has_changed)
+static int button_handler(u32_t buttons, u32_t has_changed)
 {
 }
 
@@ -155,7 +195,7 @@ static void check_rtt_command(u8_t *data, u8_t len)
 }
 #endif
 
-void test_main(void)
+void main(void)
 {
 	int err;
 	err = dk_leds_init();
@@ -167,10 +207,10 @@ void test_main(void)
 		printk("Could not initialize buttons, err code: %d\n", err);
 	}
 
-	PRINT("Starting production test - thingy:91\r\n");
+	printk("Starting production test - thingy:91\r\n");
 
         #if defined(ENABLE_RTT_CMD_GET)
-                PRINT("Waiting for test parameters...\r\n");
+                printk("Waiting for test parameters...\r\n");
                 while(!m_test_params_received)
                 {
                         if(SEGGER_RTT_HasKey())
@@ -194,28 +234,26 @@ void test_main(void)
 
 
 	dk_set_leds(LEDS_RED);
-	k_sleep(1000);
+	k_sleep(200);
 	dk_set_leds(DK_NO_LEDS_MSK);
 	dk_set_leds(LEDS_GREEN);
-	k_sleep(1000);
+	k_sleep(200);
 	dk_set_leds(DK_NO_LEDS_MSK);
 	dk_set_leds(LEDS_BLUE);
-	k_sleep(1000);
+	k_sleep(200);
 	dk_set_leds(DK_NO_LEDS_MSK);
 
-	PRINT("Got test parameters!\r\n");
-	ztest_test_suite(thingy91_production,	/* Name of test suite */
-		ztest_unit_test(test_button),
-		ztest_unit_test(ADXL372),      	/* Add tests... */
-		ztest_unit_test(BME680),
-		ztest_unit_test(ADXL362),
-		ztest_unit_test(BH1749)
-	);
 	while(1)
 	{
-		ztest_run_test_suite(thingy91_production);
+                run_test(&pca20035_ADXL372, "pca20035_ADXL372");
+                run_test(&pca20035_ADXL362, "pca20035_ADXL362");
+                run_test(&pca20035_BME680,  "pca20035_BME680");
+                run_test(&pca20035_BH1749,  "pca20035_BH1749");
+                run_test(&pca20035_test_button, "pca20035_button");
+                run_test(&pca20035_test_buzzer, "pca20035_buzzer");
 		k_sleep(100);
 		// Stop execution if test failed.
+                all_tests_succeeded ? printk("\r\nTEST SUITE SUCCESS!\r\n") : printk("\r\nTEST SUITE FAILED!\r\n");
 		if(!all_tests_succeeded) break;
 	}
 	dk_set_leds(LEDS_PATTERN_WAIT);
